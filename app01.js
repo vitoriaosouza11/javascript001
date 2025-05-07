6618fc98dd075e25b5d9a21d9674334d169b3490
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-05-06 22:05:26 -0300

    declaração de variável

---
+++ app01.js
@@ -0,0 +1,40 @@
+// Boas práticas
+// Case sensitive
+// Sintaxe básica e comentário
+// Declaração de variáveis(var, let, const)
+let nome = "Vitória"
+let idade = 21
+// Console e Debug
+
+// Operadores
+//     Operador Atribuição
+//     Operadores aritméticos
+//     Operadores relacionais
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+//     Laços de repetição
+
+// Arrays 
+
+// Funções básicas
+// Escopo
+
+
+// Alterar conteúdo e atributos *
+// incluir js em página html *
+// Eventos *
+// DOM *
+// Objeto
+// parâmetros e retorno*
+
+// Programação assíncrona***
+// API Fetch ***
+
+// JSON ****
+// funções modernas (arrow functions) ****
+
+
+// Spread e REST  ************
+ 
