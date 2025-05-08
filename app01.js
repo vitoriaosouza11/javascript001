d53fd8dc0016c243b825986e0543954c043f537a
vitoriaosouza11 <vitoriadesouza.2003@icloud.com>
2025-05-08 20:15:16 -0300

    operadores

--- app01.js
+++ app01.js
@@ -27,10 +27,31 @@ console.log(typeof salario)
 console.log(typeof fumante)  
 
 // Operadores
-//     Operador Atribuição
-//     Operadores aritméticos
-//     Operadores relacionais
-//     Operadores lógicos
+//     Operador Atribuição ( = )
+//     Operadores aritméticos (+, -, /, *)
+console.log(10+15)
+let n1 = 10
+let n2 = 5
+console.log(n1+n2) // soma
+console.log(n1-n2) // subtração
+console.log(n1*n2) // multiplicação 
+console.log(n1/n2) // divisão
+console.log(n1%3) // módulo (resto da divisão)
+
+//     Operadores relacionais ( >, <, >=, <=, ==, ===, !=, !==)
+console.log(n1==n2) // igualdade
+console.log(n1!=n2) // diferente
+console.log(n1>n2) // maior
+console.log(10<10) // menor
+console.log(10<=10) // menor igual
+console.log(150>=175) // maior igual
+
+//     Operadores lógicos ( &&, ||, !)
+console.log(!10>2) // não - inverte o resultado, se for verdadeiro muda pra falso, se for falso muda pra verdadeiro
+console.log(!false)
+console.log(10>2 && 35<100 && n1>n2 && 100<90) // E - todas as verificações precisam ser verdadeiras para o resultado ser verdadeiro.
+console.log(10<2 || 100==150 || 57==57) // OU - apenas uma verificação precisa ser verdadeira para o resultado ser verdadeiro, o resultado só será falso quando TODAS as verificações forem falsas.
+
 
 // Estrutura
 //     Estrutura de controle/decisão
